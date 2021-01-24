6ee22857ba8f079943385e8e48d8eb24b4f57142
Paolo Raez <str237@gmail.com>
2021-01-24 12:43:56 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
+use std::collections::HashMap;
 struct Solution;
 
-static LOOKUP: [usize; 10] = {
-    let mut l = [0; 10];
-
-    for i in 0..10 {
-        l[i] = i.pow(2);
-    }
-
-    l
-};
+static LOOKUP_POW: [i32; 10] = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81];
+const zero: usize = '0' as usize;
 
 impl Solution {
 
+    #[inline]
+    fn char_pow_2(c: char) -> i32 {
+        LOOKUP_POW[(c as usize) - zero]
+    }
+
     fn get_next(n: i32) -> i32 {
         let s = n.to_string();
-        //return s.chars().fold(|acc, x| 1);
-        3
+        return s.chars().fold(0, |acc, c| acc + Solution::char_pow_2(c));
     }
 
     pub fn is_happy(n: i32) -> bool {
-        true
+        let mut cache: HashMap<i32, bool> = HashMap::new();
+        let mut result = n;
+        loop {
+            if result == 1 {
+                return true
+            }
+            cache.get(k)
+        }
     }
 }
 
