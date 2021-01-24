e6eb1469be9ecc692c591b81fc18a4b298af9bea
Paolo <str237@gmail.com>
2021-01-24 19:31:25 +0100

    Done

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use std::collections::HashMap;
 struct Solution;
 
 static LOOKUP_POW: [i32; 10] = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81];
-const zero: usize = '0' as usize;
+const ZERO: usize = '0' as usize;
 
 impl Solution {
 
     #[inline]
     fn char_pow_2(c: char) -> i32 {
-        LOOKUP_POW[(c as usize) - zero]
+        LOOKUP_POW[(c as usize) - ZERO]
     }
 
     fn get_next(n: i32) -> i32 {
@@ -23,12 +23,16 @@ impl Solution {
             if result == 1 {
                 return true
             }
-            cache.get(k)
+            let visited = cache.get(&result).unwrap_or(&false).clone();
+            if visited {
+                return false
+            } 
+            cache.insert(result, true);
+            result = Solution::get_next(result);
         }
     }
 }
 
 fn main() {
-    println!("{}", '1' as u32);
-    println!("{}", Solution::is_happy(19));
+    println!("{}", Solution::is_happy(2));
 }
